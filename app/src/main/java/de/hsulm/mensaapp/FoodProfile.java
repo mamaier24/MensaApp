0f21b29ce0099506b8a668f77481cffbe63572b3
Felix Hermann <hermannfel@gmail.com>
2018-12-10 21:28:51 +0100

    Felix Image Slider

--- app/src/main/java/de/hsulm/mensaapp/FoodProfile.java
+++ app/src/main/java/de/hsulm/mensaapp/FoodProfile.java
@@ -1,19 +1,31 @@
 package de.hsulm.mensaapp;
 
+import android.database.sqlite.SQLiteException;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.DefaultSliderView;
+import com.daimajia.slider.library.Transformers.BaseTransformer;
+
 import java.io.InputStream;
 
 import static de.hsulm.mensaapp.R.layout.activity_food_profile;
+import static de.hsulm.mensaapp.R.layout.indicator_layout;
 
 public class FoodProfile extends AppCompatActivity {
+    private SliderLayout sliderShow;
+    private DefaultSliderView defaultSliderView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +35,9 @@ public class FoodProfile extends AppCompatActivity {
         TextView mBewertung = (TextView) findViewById(R.id.Bewertung);
         TextView mTitel = (TextView) findViewById(R.id.Titel);
         TextView mPreis = (TextView) findViewById(R.id.Preis);
-        ImageView mImage = (ImageView) findViewById(R.id.Bild) ;
         RatingBar mRatingBar = (RatingBar)findViewById(R.id.ratingBar2);
+        sliderShow = (SliderLayout)findViewById(R.id.imageSlider);
+        defaultSliderView = new DefaultSliderView(this);
 
         FoodClass food = getIntent().getParcelableExtra("food");
 
@@ -34,7 +47,7 @@ public class FoodProfile extends AppCompatActivity {
         String imageRes = food.getmimgId();
 
 
-        new DownloadProfileImage(mImage).execute(Constants.ROOT_URL_PICTURES + imageRes);
+        new DownloadProfileImage().execute(Constants.ROOT_URL_PICTURES + imageRes);
 
         mRatingBar.setRating(Float.parseFloat(rating));
 
@@ -47,31 +60,45 @@ public class FoodProfile extends AppCompatActivity {
     }
 
 
-    private class DownloadProfileImage extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
+    private class DownloadProfileImage extends AsyncTask<String, Void, Void> {
+        protected Void doInBackground(String... urls) {
 
-        public DownloadProfileImage(ImageView bmImage) {
-            this.bmImage = bmImage;
-        }
-
-        protected Bitmap doInBackground(String... urls) {
-            String urldisplay = urls[0];
-            Bitmap mIcon11 = null;
             try {
-                InputStream in = new java.net.URL(urldisplay).openStream();
-                mIcon11 = BitmapFactory.decodeStream(in);
+                if (urls.length <2 ){
+                    defaultSliderView
+                            .image(urls[0])
+                            .setScaleType(BaseSliderView.ScaleType.Fit);
+                    sliderShow.addSlider(defaultSliderView);
+                    sliderShow.stopAutoCycle();
+                    sliderShow.setPagerTransformer(false, new BaseTransformer() {
+                        @Override
+                        protected void onTransform(View view, float position) {
+                        }
+                    });
+                }
+                else {
+                    for (int i = 0; i<urls.length;i++){
+                        defaultSliderView
+                                .image(urls[i])
+                                .setScaleType(BaseSliderView.ScaleType.Fit);
+                        sliderShow.addSlider(defaultSliderView);
+                        sliderShow.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+                    }
+                }
+
             } catch (Exception e) {
-                mIcon11 = null;
+                defaultSliderView
+                        .image(R.drawable.ic_restaurant_menu_black_24dp)
+                        .setScaleType(BaseSliderView.ScaleType.Fit);
+                sliderShow.addSlider(defaultSliderView);
+                Log.d("Error","Image Download failed");
             }
-            return mIcon11;
+
+            sliderShow.setDuration(7500);
+            return null;
         }
 
-        protected void onPostExecute(Bitmap result) {
-            if(result != null) {
-                bmImage.setImageBitmap(result);
-                bmImage.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            }
-        }
+
     }
 
 }
