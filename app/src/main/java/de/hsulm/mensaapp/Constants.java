6a3579105421c5d2a583577239711500464fa091
stda1991 <45353326+stda1991@users.noreply.github.com>
2018-12-17 21:40:47 +0100

    Cleaned Constants

--- app/src/main/java/de/hsulm/mensaapp/Constants.java
+++ app/src/main/java/de/hsulm/mensaapp/Constants.java
@@ -6,14 +6,14 @@ package de.hsulm.mensaapp;
 public class Constants {
 
     public static final String ROOT_URL = "http://www.s673993392.online.de/v1/";
-    public static final String ROOT_URL_DB_OPS = "http://www.s673993392.online.de/v2/";
     public static final String ROOT_URL_PICTURES = "http://www.s673993392.online.de/pictures/";
-    public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
-    public static final String URL_LOGIN = ROOT_URL + "userLogin.php";
-    public static final String URL_DB_OPS = ROOT_URL_DB_OPS + "index.php";
+
+    public static final String URL_REGISTER = ROOT_URL + "user_register.php";
+    public static final String URL_LOGIN = ROOT_URL + "user_login.php";
+    public static final String URL_DB_OPS = ROOT_URL + "search_by_id.php";
     public static final String URL_RATING = ROOT_URL + "user_rating.php";
     public static final String URL_GET_RATING = ROOT_URL + "get_user_rating.php";
-    public static final String URL_IMG_UPLOAD = ROOT_URL + "img_upload.php";
+    public static final String URL_IMG_UPLOAD = ROOT_URL + "upload_img.php";
     public static final String URL_IMG_FETCH = ROOT_URL + "fetch_img.php";
 
 }
